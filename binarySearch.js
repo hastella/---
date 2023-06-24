cc4cd9022cea3b46ffcabc0f4715414231bf831d
Stella_Ha <sangyeeh@gmail.com>
2023-06-24 21:51:16 +0900

    :memo: binary search 이진탐색
    : binary search 이진탐색

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+const array = [1, 1, 5, 124, 400, 599, 1004, 2876, 8712];
+
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+  let mid = Math.floor((left + right) / 2);
+  while (left < right) {
+    if (array[mid] === target) {
+      return mid;
+    }
+    if (array[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+    mid = Math.floor((left + right) / 2);
+  }
+  return -1;
+}
+
+console.log(binarySearch(array, 2876)); // 7
+console.log(binarySearch(array, 1)); // 1
+console.log(binarySearch(array, 500)); // -1
